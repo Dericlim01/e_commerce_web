a58418ad613384a445927245baa9c580a57bd50f
LimSY20 <dericlimtech@gmail.com>
2026-08-13 00:19:06 +0800

    db: update User model with customer details and generate EF migration

--- e_commerce_web/Models/User.cs
+++ e_commerce_web/Models/User.cs
@@ -5,5 +5,9 @@ namespace e_commerce_web.Models
         public int Id { get; set; }
         public string Email { get; set; } = string.Empty;
         public string PasswordHash { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public DateTime MemberSince { get; set; } = DateTime.Now;
     }
 }
