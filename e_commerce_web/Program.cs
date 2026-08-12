79dabe1f8daebc54ef7298732e903367ef3bef30
LimSY20 <dericlimtech@gmail.com>
2026-08-13 00:19:25 +0800

    fix: resolve PostgreSQL connection string issue and auto-apply migrations on startup

--- e_commerce_web/Program.cs
+++ e_commerce_web/Program.cs
@@ -22,10 +22,16 @@ builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationSc
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
+    app.UseDeveloperExceptionPage();
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
