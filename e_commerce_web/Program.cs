c164b5b76fefc76ed98d2aad2107811d643e6bf5
LimSY20 <dericlimtech@gmail.com>
2026-08-12 13:46:49 +0800

    feat: login button, database connection
    - button will change to user name if user logged in
    - add logout button
    - add database connection and cookie authentication

--- e_commerce_web/Program.cs
+++ e_commerce_web/Program.cs
@@ -1,7 +1,24 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using e_commerce_web.Data;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+// Register Cookie Authentication
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Login/Login"; // Redirect to login page if not authenticated
+        options.LogoutPath = "/Login/Logout"; // Redirect to logout page
+        options.AccessDeniedPath = "/Login/AccessDenied"; // Redirect to access denied page
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(30); // Set cookie expiration time
+    });
 
 var app = builder.Build();
 
